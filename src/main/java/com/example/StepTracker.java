4bd14146104b2ed07f00f8ed9198b701dfac56fa
jinasun <ginasuns60916@gmail.com>
2023-12-14 11:20:15 +0800

    trew

--- src/main/java/com/example/StepTracker.java
+++ src/main/java/com/example/StepTracker.java
@@ -4,22 +4,38 @@ public class StepTracker{
 
     private int minStep;
     private int activeDays;
-    private int ab;
+    private int step;
+    private int total;
+    private int day;
+    // private int ;
 
 
     public StepTracker(int minStep){
         this.minStep = minStep;
+        int step = 0;
+        int activeDays = 0;
+        int total = 0;
+        int day = 0;
     }
 
-    public void addDailySteps(int active) {
-        minStep += activeDays();
-
+    public void addDailySteps(int step) {
+        if (step >= minStep){
+            activeDays++;
+        }
+        total += step;
+        day++;
     }
 
     public int activeDays(){
         return activeDays;
     }
 
+    public double averageSteps(){
+        if (day == 0){
+            return 0;
+        }
+        return total/day;
+    }
 
 }
 
