4627ec00711a8a160855142a13ffadb9c1fb5107
jinasun <ginasuns60916@gmail.com>
2023-12-14 10:33:46 +0800

    dfghjkl

--- src/main/java/com/example/StepTracker.java
+++ src/main/java/com/example/StepTracker.java
@@ -1,3 +1,26 @@
 package com.example;
 
+public class StepTracker{
+
+    private int minStep;
+    private int activeDays;
+    private int ab;
+
+
+    public StepTracker(int minStep){
+        this.minStep = minStep;
+    }
+
+    public void addDailySteps(int active) {
+        minStep += activeDays();
+
+    }
+
+    public int activeDays(){
+        return activeDays;
+    }
+
+
+}
+
 //write your class implementation here
